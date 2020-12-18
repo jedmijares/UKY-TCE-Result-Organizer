58de0b8dcedc24e11b5c06e407e5ca9d2b0f4d2d
Jed <jed@mijar.es>
2020-12-18 14:12:00 -0600

    gets pages

--- TCE-Results.py
+++ TCE-Results.py
@@ -1,3 +1,8 @@
 import textract
-text = textract.process('Summer 2018 Public Results.pdf')
-print(text.decode("utf-8"))
+text = (textract.process('Summer 2018 Public Results.pdf')).decode("utf-8")
+# text = (textract.process('Fall 2015 - 2016 WEB.pdf')).decode("utf-8")
+
+pages = text.split(chr(12)) # this char splits pages
+for chunk in pages:
+    print(chunk)
+    print("000000000000000000000")
